4d90c5e960b73cf79ab30fad1df38e86404d53dd
majaliju <majalijunyc@gmail.com>
2022-05-14 16:12:01 -0400

    updated

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,8 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+
+  // checking if the cart is populated (> 0; true) or empty (=0; false)
+  const [inCart, setInCart] = useState(true)
+
+  // this function flips the switch and produces the opposite Boolean value
+// eslint-disable-next-line
+  function putInCart() {
+    return setInCart((inCart) => !inCart)
+  }
+
+  // if className = inCart, it'll read "in-cart"
+  // otherwise, it'll show an empty cart, where inCart = false
+  const className = inCart ? "in-cart" : ""
+
+
   return (
-    <li className="">
+    <li className={className}>
       <span>{name}</span>
       <span className="category">{category}</span>
       <button className="add">Add to Cart</button>
