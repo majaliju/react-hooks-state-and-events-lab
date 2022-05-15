3c98a32d2ab3379f24917749caeeaae3a4976659
majaliju <majalijunyc@gmail.com>
2022-05-15 00:19:22 -0400

    updated, 3rd deliverable

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [selectedCategory, setSelectedCategory] = useState("All")
+
+  function handleChange(e) {
+    console.log(e.target.value)
+  }
+
+
+  // // my main issue is with this function here; 
+  // // it's def not written properly but something here is 
+  // // where I need to push forth on 
+  // const itemsToDisplay = items.filter((item) => {
+  //   // logic here was just re-factored from a previous lab
+  //   if (selectedCategory === "All"){
+  //     return true;
+  //   } else {
+  //     return item.value === setSelectedCategory;
+  //   }
+  // })
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
@@ -14,7 +33,7 @@ function ShoppingList({ items }) {
       </div>
       <ul className="Items">
         {items.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
+          <Item key={item.id} onChange={handleChange} name={item.name} category={item.category} />
         ))}
       </ul>
     </div>
